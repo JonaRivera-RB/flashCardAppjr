e13e54cbe5d3704a3d3b4e7921ee13bbc8b91063
Jonathan Rivera <jonathanriveramb@gmail.com>
2018-09-23 23:23:03 -0500

    mejoras en el codigo, implementamos niveles

--- flashCards/flashCards/controlladores/scoreVC.swift
+++ flashCards/flashCards/controlladores/scoreVC.swift
@@ -15,11 +15,18 @@ class scoreVC: UIViewController {
     @IBOutlet weak var incorrectAnswerLbl: UILabel!
     @IBOutlet weak var learnedLbl: UILabel!
     @IBOutlet weak var viewlevel: UIView!
+    @IBOutlet weak var yourPoints: UILabel!
+    @IBOutlet weak var progressLevel: UIView!
     
     var correct = 0
     var incorrect = 0
     var learned = 0
+    var levelPts = 20
+    var myPts = 30
     
+    var startValue: Int = 0
+    
+    var displayLink: CADisplayLink?
      let shapeLayer = CAShapeLayer()
     
     //conexion con el maaged context para la base de datos
@@ -34,8 +41,37 @@ class scoreVC: UIViewController {
         correctAnswereLbl.text = String(correct)
         incorrectAnswerLbl.text = String(incorrect)
         learnedLbl.text = String(learned)
-
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        displayLink = CADisplayLink(target: self, selector: #selector(handleUpdate))
+        displayLink?.add(to: .main, forMode: .default)
+    }
+    
+    //buscamos en cada una de las constrains de la abrra de progresos
+    //si el identificador del constrainr es igual a barWidht
+    //entonces cambio el valor
+    @objc func handleUpdate() {
+        yourPoints.text = String("\(startValue) / \(levelPts)")
+        for constraint in self.progressLevel.constraints {
+            if constraint.identifier == "levelwidth" {
+                 constraint.constant = (self.view.frame.size.width * 0.80)/CGFloat(self.levelPts) * CGFloat(self.startValue)
+            }
+        }
+        startValue += 1
+        //si valor inicial es mayor o igual que los puntos requeridos para subir de nivel
+        //restame los puntos, subeme de nivel y sube puntos para obtener nivel nuevo
+        if startValue >= levelPts {
+            startValue = 0
+            myPts -= levelPts
+            levelPts = 30
+            
+        }
+        if startValue > myPts {
+            startValue = myPts
+        }
+    }
+    
     @IBAction func backWasPressed(_ sender: Any) {
         let rootViewGame = self.storyboard?.instantiateViewController(withIdentifier: "viewMain")
         self.present(rootViewGame!, animated: true, completion: nil)
@@ -43,6 +79,7 @@ class scoreVC: UIViewController {
     @IBAction func tryAgainWasPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
     func getData( correct:Int, incorrect:Int, learned: Int)
     {
         self.correct = correct
